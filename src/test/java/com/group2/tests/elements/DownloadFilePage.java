17d57888fcbe0aef63bc22a3d5ffc87666680377
NastyaAshurok <roxi2008@mail.ru>
2020-11-16 12:11:11 +0300

    added frames test

--- src/test/java/com/group2/tests/elements/DownloadFilePage.java
+++ src/test/java/com/group2/tests/elements/DownloadFilePage.java
@@ -16,23 +16,6 @@ public class DownloadFilePage {
     SelenideElement downloadTxtFile = $(By.xpath("//div[@class='example']//a[contains(@href,'download/some-file.txt')]"));
 
     public void downloadFile() throws FileNotFoundException {
-   /*     // System.setProperty("webdriver.somefile.text", "C:\\some-file.txt"); // нужна ли эта строчка?
-        String baseUrl = "https://the-internet.herokuapp.com/download";
-        WebDriver driver = new ChromeDriver();
-        driver.get(baseUrl);
-        WebElement downloadButton = driver.findElement(By.name("https://the-internet.herokuapp.com/download/some-file.txt")); //how to find this link?
-        String sourceLocation = downloadButton.getAttribute("href");
-        String wget_command = "cmd /c C:\\Wget\\wget.exe -P D: --no-check-certificate " + sourceLocation;
-
-        try {
-            Process exec = Runtime.getRuntime().exec(wget_command);
-            int exitVal = exec.waitFor();
-            System.out.println("Exit value: " + exitVal);
-        } catch (InterruptedException | IOException ex) {
-            System.out.println(ex.toString());
-        }
-        driver.close();
-    } */
         Configuration.reportsFolder = "C:\\Wget\\";
         open("https://the-internet.herokuapp.com/download");
         downloadTxtFile.download(3000, withExtension("txt"));
