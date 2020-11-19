66ff72d60fbd8d739cf916ec25f9f2c57b32afc8
NastyaAshurok <roxi2008@mail.ru>
2020-11-19 12:44:12 +0300

    added SwitchTo

--- src/test/java/com/group2/tests/elements/FramesPage.java
+++ src/test/java/com/group2/tests/elements/FramesPage.java
@@ -11,10 +11,22 @@ public class FramesPage {
     SelenideElement iFrameElement= $(By.xpath("//div[@class='example']//a[contains(@href,'/iframe')]"));
     SelenideElement closePopUpButton= $(By.xpath("//button[@role='presentation']"));
     SelenideElement textBody= $(By.xpath("//iframe[@id='mce_0_ifr']"));
+    private By iFrame = By.id("mce_0_ifr");
+    private By textBodyFrame = By.id("tinymce");
+    private By returnValueBackButton = By.xpath("//button[@role='c']//i[contains(@class,'mce-i-undo')]");
+
     public void iFrameAddText(String textForInput) {
         open("https://the-internet.herokuapp.com/frames");
         iFrameElement.click();
         closePopUpButton.click();
-        textBody.setValue(textForInput);
+        switchTo(iFrame); ///???
+        $(textBodyFrame).val(textForInput);
+        $(returnValueBackButton).click();
+        //textBody.setValue(textForInput);
     }
+
+    /* private void switchTo(By iFrame) {
+
+
+    }*/
 }
